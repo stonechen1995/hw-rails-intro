1cd5d740c980cf7a7f9e62b981284c99f7281871
Ubuntu <ubuntu@ip-172-31-16-105.ca-central-1.compute.internal>
2022-03-12 15:36:55 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,9 +8,17 @@ class MoviesController < ApplicationController
   
   def index
     @sort = params[:sort] || session[:sort]
+    if !(Movie.column_names.include?(@sort)) 
+      @sort = session[:sort]
+    end
     session[:sort] = @sort 
     @all_ratings = Movie.all_ratings
     params[:ratings].nil? ? @selected_ratings = session[:ratings] : @selected_ratings = params[:ratings].keys
+    @selected_ratings.each do |rate|
+      if !(@all_ratings.include?(rate)) 
+        @selected_ratings = session[:ratings];
+      end
+    end
     session[:ratings] = @selected_ratings
     @movies = Movie.where(rating: @selected_ratings).order(@sort)
     @hilite = @sort
